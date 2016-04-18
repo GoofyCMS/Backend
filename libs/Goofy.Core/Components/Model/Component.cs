ac7a79489399d16c3627552b908ae774056d8257
Adrian Leon Morell <a.leon2@lab.matcom.uh.cu>
2016-04-17 23:45:25 -0400

    IEquatable<Component> interface implementation added for Component class.

--- libs/Goofy.Core/Components/Model/Component.cs
+++ libs/Goofy.Core/Components/Model/Component.cs
@@ -1,13 +1,19 @@
-﻿using Goofy.Core.Entity.Base;
+﻿using System;
+using Goofy.Core.Entity.Base;
 
 namespace Goofy.Core.Components
 {
-    public class Component : GoofyEntityBase
+    public class Component : GoofyEntityBase, IEquatable<Component>
     {
         public int ComponentId { get; set; }
         public string FullName { get; set; }
         public string Version { get; set; }
         public bool Installed { get; set; }
         public bool IsSystemComponent { get; set; }
+
+        public bool Equals(Component other)
+        {
+            return GlobalId == other.GlobalId;
+        }
     }
 }
