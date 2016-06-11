efaa8cb183d773b82b8748ffe03e4e4d704ca8e4
Adrian Leon Morell <a.leon2@lab.matcom.uh.cu>
2016-06-11 17:29:47 -0400

    Invalid Context require fixed

--- src/Goofy.Presentation.Blog/Providers/BlogContextProvider.cs
+++ src/Goofy.Presentation.Blog/Providers/BlogContextProvider.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using Goofy.Domain.Core.Service.Adapter;
-using Goofy.Infrastructure.Blog.Data;
 using Goofy.Presentation.Core.Providers;
+using Goofy.Domain.Blog.Service.Data;
 
 namespace Goofy.Presentation.Blog.Providers
 {
     public class BlogContextProvider : BaseContextProvider<BlogMetadataContext>
     {
         public BlogContextProvider(IServiceProvider services, ITypeAdapterFactory typeAdapterFactory)
-            : base(services, services.GetRequiredService<BlogContext>(), typeAdapterFactory)
+            : base(services, services.GetRequiredService<IBlogUnitOfWork>(), typeAdapterFactory)
         {
         }
     }
