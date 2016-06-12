3555cfd426b453b63fc24e9e682fe80a8ec7fffb
Adrian Leon Morell <a.leon2@lab.matcom.uh.cu>
2016-06-12 00:43:24 -0400

    Goofy.Application.Administration.DTO project added....

---
+++ src/Goofy.Application.Administration.DTO/RoleItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goofy.Application.Administration.DTO
+{
+    public class RoleItem
+    {
+    }
+}
