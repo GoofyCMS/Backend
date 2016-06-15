af4cf56e5d1927504e62769f8164ee6334be20b2
Adrian Leon Morell <a.leon2@lab.matcom.uh.cu>
2016-06-15 10:24:16 -0400

    fixing bug about GoofyActionSelector

---
+++ src/Goofy.Application.Core/Extensions/PluginAssemblyProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using Goofy.Application.Core.Abstractions;
+using System.Linq;
+using System.Reflection;
+
+namespace Goofy.Application.Core.Extensions
+{
+    public static class PluginAssemblyProviderExtensions
+    {
+        public static string GetPluginContainingAssembly(this IPluginAssemblyProvider assemblyProvider, Assembly assembly)
+        {
+            return assemblyProvider.PluginAssemblies.Where(kvp => kvp.Value.Contains(assembly))
+                .Select(kvp => kvp.Key)
+                .FirstOrDefault();
+        }
+    }
+}
--- src/Goofy.Presentation/Services/GoofyActionSelector.cs
+++ src/Goofy.Presentation/Services/GoofyActionSelector.cs
@@ -1,5 +1,6 @@
 ﻿using Goofy.Application.Administration.Extensions;
 using Goofy.Application.Administration.Services.Abstractions;
+using Goofy.Application.Core.Extensions;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.ActionConstraints;
 using Microsoft.AspNet.Mvc.Controllers;
@@ -8,6 +9,7 @@ using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Goofy.Presentation.Services
 {
@@ -33,12 +35,22 @@ namespace Goofy.Presentation.Services
 
         private List<ActionDescriptor> FilterOnlyActiveActions(IReadOnlyList<ActionDescriptor> actions)
         {
+
+            if (actions == null)
+                return null;
+            var actionController = actions.Where(a => a is ControllerActionDescriptor).Cast<ControllerActionDescriptor>();
+            if (actionController.Count() == 0)
+                return null;
+            var pluginNames = GetPluginForValidActionDescriptors(actionController).ToArray();
+            var plugins = _pluginManager.Plugins.GetAll(a => pluginNames.Contains(a.Name)).ToArray();//Ver cuantas queries hace esto
+
+            actionController.Select(a => new { Action = a, PluginName = _pluginManager.PluginAssemblyProvider.GetPluginContainingAssembly(a.ControllerTypeInfo.Assembly) });
             return actions?.Where(ActionsInActiveComponents).ToList();
         }
 
         private bool ActionsInActiveComponents(ActionDescriptor actionDescriptor)
         {
-            var controllerActionDescriptor = actionDescriptor as ControllerActionDescriptor;
+            var controllerActionDescriptor = ;
             if (controllerActionDescriptor != null)
             {
                 var assembly = controllerActionDescriptor.ControllerTypeInfo.Assembly;
@@ -47,5 +59,13 @@ namespace Goofy.Presentation.Services
             }
             return true;
         }
+
+        public IEnumerable<string> GetPluginForValidActionDescriptors(IEnumerable<ActionDescriptor> actions)
+        {
+            return actions.Where(a => a is ControllerActionDescriptor)
+                .Cast<ControllerActionDescriptor>()
+                .Select(a => _pluginManager.PluginAssemblyProvider.GetPluginContainingAssembly(a.ControllerTypeInfo.Assembly));
+        }
+
     }
 }
