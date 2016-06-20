d7c838147355a152cf82e1cec624c63fc20928c8
Adrian Leon Morell <a.leon2@lab.matcom.uh.cu>
2016-06-20 19:47:55 -0400

    User registration added

---
+++ src/Goofy.Presentation.Administration/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Identity;
+using Goofy.Presentation.Administration.ViewModels;
+using Goofy.Domain.Administration.Entity;
+using System.Linq;
+using Microsoft.AspNet.Authorization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Goofy.Presentation.Administration.Controllers
+{
+    [Authorize(Policy = "RequireCreateGoofyUser")]
+    [Route("api/administration")]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<GoofyUser> _userManager;
+
+        public RegisterController(UserManager<GoofyUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new GoofyUser
+                {
+                    UserName = loginViewModel.Username,
+                    Email = loginViewModel.Username,
+                };
+                var result = _userManager.CreateAsync(user, loginViewModel.Password).Result;
+                return new ObjectResult(new { success = result.Succeeded, errors = result.Errors.Select(e => e.Description) });
+            }
+            return new BadRequestResult();
+        }
+    }
+}
---
+++ src/Goofy.Presentation.Administration/ViewModel/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Goofy.Presentation.Administration.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
