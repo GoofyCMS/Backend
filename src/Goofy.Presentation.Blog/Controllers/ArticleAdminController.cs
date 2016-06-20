618f274aaa88a9b2eed42073c34de832a4aade4b
Adrian Leon Morell <a.leon2@lab.matcom.uh.cu>
2016-06-20 12:31:47 -0400

    ArticlePublicController added. This is the controller used for public site

--- src/Goofy.Presentation.Blog/Controllers/ArticleAdminController.cs
+++ src/Goofy.Presentation.Blog/Controllers/ArticleAdminController.cs
@@ -9,9 +9,9 @@ using Microsoft.AspNet.Authorization;
 namespace Goofy.Presentation.Blog.Controllers
 {
     [Route("api/blogadmin/ArticleItems")]
-    public class BlogController : BaseController<Article, ArticleItem, int>
+    public class ArticleAdminController : BaseController<Article, ArticleItem, int>
     {
-        public BlogController(IBlogServiceMapper<Article, ArticleItem> serviceMapper, BlogContextProvider provider)
+        public ArticleAdminController(IBlogServiceMapper<Article, ArticleItem> serviceMapper, BlogContextProvider provider)
             : base(serviceMapper, provider)
         {
         }
---
+++ src/Goofy.Presentation.Blog/Controllers/ArticlePublicController.cs
@@ -0,0 +1,18 @@
+﻿using Goofy.Application.Blog.DTO;
+using Goofy.Domain.Blog.Entity;
+using Goofy.Domain.Blog.Service.Adapter;
+using Goofy.Presentation.Blog.Providers;
+using Goofy.Presentation.Core.Controllers;
+using Microsoft.AspNet.Mvc;
+
+namespace Goofy.Presentation.Blog.Controllers
+{
+    [Route("api/blogpublic/ArticleItems")]
+    public class ArticlePublicController : BaseReadOnlyController<Article, ArticleItem, int>
+    {
+        public ArticlePublicController(IBlogServiceMapper<Article, ArticleItem> serviceMapper, BlogContextProvider provider)
+            : base(serviceMapper, provider)
+        {
+        }
+    }
+}
